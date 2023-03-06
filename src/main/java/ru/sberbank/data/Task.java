c68a349006685d84200a2643f2eaf3069ae18ce2
Arthur Kolychev <aalkolychev@sberbank.ru>
2023-03-06 12:06:53 +0300

    homework_9

---
+++ src/main/java/ru/sberbank/data/Task.java
@@ -0,0 +1,57 @@
+package ru.sberbank.data;
+
+public class Task {
+    public int id;
+    public boolean isDeveloped;
+    public boolean isTested;
+    public String summary;
+
+    public Task(int id, String summary) {
+        this.id = id;
+        this.isDeveloped = false;
+        this.isTested = false;
+        this.summary = summary;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", isDeveloped=" + isDeveloped +
+                ", summary='" + summary + '\'' +
+                ", isTested=" + isTested + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final Task task = (Task) o;
+
+        if (id != task.id) {
+            return false;
+        }
+        if (isDeveloped != task.isDeveloped) {
+            return false;
+        }
+        if (isTested != task.isTested) {
+            return false;
+        }
+        return summary.equals(task.summary);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (isDeveloped ? 1 : 0);
+        result = 31 * result + (isTested ? 1 : 0);
+        result = 31 * result + summary.hashCode();
+        return result;
+    }
+}
---
+++ src/test/java/ru/sberbank/service/DeveloperServiceTest.java
@@ -0,0 +1,33 @@
+package ru.sberbank.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.sberbank.data.Developer;
+
+class DeveloperServiceTest {
+    private DeveloperService service = new DeveloperService();
+    @Test
+    void createTest_successful() {
+        Developer expected = new Developer(1, "Ivan", "Ivanov");
+
+        Developer actual = service.create(1, "Ivan", "Ivanov");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void createTest_failValidation(){
+        String expectedMessage = "Входные данные не валидны";
+
+        Exception actualException = Assertions.assertThrows(
+                IllegalStateException.class,
+                () -> {
+                    service.create(0, "", "");
+                }
+        );
+
+        Assertions.assertEquals(actualException.getMessage(), expectedMessage);
+    }
+
+
+}
